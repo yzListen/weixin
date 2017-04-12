31882eb9f108289e3a5f75290882781fc3789659
yzListen <912536901@qq.com>
2017-04-12 22:52:33 +0800

    init project

---
+++ pages/index/index.js
@@ -0,0 +1,15 @@
+Page({
+  data: {
+    imgUrls: [
+      '../../images/banner-1.jpg',
+      '../../images/banner-2.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 1000,
+    duration: 500
+  },
+ 
+  
+
+})

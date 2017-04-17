d18fcc4dccbba7f3a485806d2eed578fe7cf1fc1
yzListen <912536901@qq.com>
2017-04-17 21:53:34 +0800

    modified index page and added detail page

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,6 +4,34 @@ Page({
       '../../images/banner-1.jpg',
       '../../images/banner-2.jpg'
     ],
+    classlist:[
+      {
+        "name":"第一课:小程序入门准备",
+        "url":"pages/detail/detail.wxml",
+        "img":"../../images/cover-1.jpg",
+        "text":"介绍什么是小程序，小程序的实现原理，以及开发环境的部署。",
+        "teacher":"主讲老师:张德检"
+        
+      },
+      {
+        "name":"第二课:小程序入门准备",
+        "img":"../../images/cover-2.jpg","url":"/pages/detail/detail?id=1",
+        "text":"介绍什么是小程序，小程序的实现原理，以及开发环境的部署。",
+        "teacher":"主讲老师:张德检"
+      },
+      {
+        "name":"第三课:小程序入门准备",
+        "img":"../../images/cover-3.jpg","url":"/pages/detail/detail?id=1",
+        "text":"介绍什么是小程序，小程序的实现原理，以及开发环境的部署。",
+        "teacher":"主讲老师:张德检"
+      },
+      {
+        "name":"第四课:小程序入门准备",
+        "img":"../../images/cover-4.jpg","url":"/pages/detail/detail?id=1",
+        "text":"介绍什么是小程序，小程序的实现原理，以及开发环境的部署。",
+        "teacher":"主讲老师:张德检"
+      }
+    ],
     indicatorDots: true,
     autoplay: true,
     interval: 1000,

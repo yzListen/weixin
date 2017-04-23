a4c826016a8f1e71eb0c8e0c1e43345014f9a331
yzListen <912536901@qq.com>
2017-04-23 22:49:57 +0800

    detailed"detail"&"index"from psd(unfinished
    cant find"download"button,part of“appre”unfinished,font-family
    unfinished

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -3,7 +3,9 @@ Page({
   data:{
     src:"http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400"
   },
-  onLoad:function(options){
+  onLoad:function(options)
+  {
+
     // 页面初始化 options为页面跳转所带来的参数
   },
   onReady:function(){
--- pages/index/index.js
+++ pages/index/index.js
@@ -10,26 +10,30 @@ Page({
         "url":"pages/detail/detail.wxml",
         "img":"../../images/cover-1.jpg",
         "text":"介绍什么是小程序，小程序的实现原理，以及开发环境的部署。",
-        "teacher":"主讲老师:张德检"
+        "teacher":"主讲老师:张德检",
+        "id":"1",
         
       },
       {
         "name":"第二课:小程序入门准备",
         "img":"../../images/cover-2.jpg","url":"/pages/detail/detail?id=1",
         "text":"介绍什么是小程序，小程序的实现原理，以及开发环境的部署。",
-        "teacher":"主讲老师:张德检"
+        "teacher":"主讲老师:张德检",
+        "id":"2",
       },
       {
         "name":"第三课:小程序入门准备",
         "img":"../../images/cover-3.jpg","url":"/pages/detail/detail?id=1",
         "text":"介绍什么是小程序，小程序的实现原理，以及开发环境的部署。",
-        "teacher":"主讲老师:张德检"
+        "teacher":"主讲老师:张德检",
+        "id":"3",
       },
       {
         "name":"第四课:小程序入门准备",
         "img":"../../images/cover-4.jpg","url":"/pages/detail/detail?id=1",
         "text":"介绍什么是小程序，小程序的实现原理，以及开发环境的部署。",
-        "teacher":"主讲老师:张德检"
+        "teacher":"主讲老师:张德检",
+        "id":"4",
       }
     ],
     indicatorDots: true,

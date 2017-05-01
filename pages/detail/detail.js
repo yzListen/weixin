934cbbbffd6ccafd40f441186a4f3994515f6ed8
yzListen <912536901@qq.com>
2017-05-01 13:40:27 +0800

    add "login"css,finished "like"botton in "detail"

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,7 +1,9 @@
 // pages/detail/detail.js
 Page({
   data:{
-    src:"http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400"
+    src:"http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400",
+    like:"../../images/icon-heart.png",
+    tig:false,
   },
   onLoad:function(options)
   {
@@ -19,5 +21,10 @@ Page({
   },
   onUnload:function(){
     // 页面关闭
-  }
+  },
+  like:function(){
+    
+    if(!this.data.tig)(this.setData({like:"../../images/icon-heart-a.png",tig:true}))
+    else(this.setData({like:"../../images/icon-heart.png",tig:false}))
+},
 })
